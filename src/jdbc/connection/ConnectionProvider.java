f4d47265e5b2c2328b967539ffb7daff2c9a9eca
dltjddn0613r <dltjddn0613r@gmail.com>
2024-07-14 17:35:53 +0900

    Update ConnectionProvider.java
    테스트중

--- src/jdbc/connection/ConnectionProvider.java
+++ src/jdbc/connection/ConnectionProvider.java
@@ -4,8 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionProvider {
-
+public class ConnectionProvider {//수정
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(
                 "jdbc:apache:commons:dbcp:board");
